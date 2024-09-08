e158343bf60f2d5eb40f3b5cba28079f2bfeb84a
Grifonhard <142656587+Grifonhard@users.noreply.github.com>
2024-09-08 20:24:07 +0300

    добавил флаги

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"flag"
 
 	"github.com/Grifonhard/Practicum-metrics/internal/storage"
 	web "github.com/Grifonhard/Practicum-metrics/internal/web_server"
@@ -10,6 +11,10 @@ import (
 )
 
 func main() {
+	port := flag.String("a", "8080", "server port")
+
+	flag.Parse()
+
 	stor := storage.New()
 
 	r := gin.Default()
@@ -19,6 +24,6 @@ func main() {
 	r.GET("/value/:type/:name", web.Middleware(), web.Get(stor))
 	r.GET("/", web.List(stor))
 
-	fmt.Printf("Server start localhost:8012")
-	log.Fatal(r.Run(":8012"))
+	fmt.Printf("Server start localhost:%s\n", *port)
+	log.Fatal(r.Run(":" + *port))
 }
