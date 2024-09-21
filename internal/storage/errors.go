fbbcdee0f1aadb31db266e28279418bc2456529f
Grifonhard <142656587+Grifonhard@users.noreply.github.com>
2024-09-21 10:35:26 +0300

    фикс

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -3,10 +3,10 @@ package storage
 import "errors"
 
 var (
-	MetricEmpty              = errors.New("metric is empty")
-	MetricTypeUnknown        = errors.New("unknown metrics type")
-	MetricNoData             = errors.New("no data for this metric")
-	MetricValEmptyField      = errors.New("empty field in metrics")
-	MetricValWrongType       = errors.New("wrong type of metrics")
-	MetricValValueIsNotFloat = errors.New("value is not float64")
+	ErrMetricEmpty              = errors.New("metric is empty")
+	ErrMetricTypeUnknown        = errors.New("unknown metrics type")
+	ErrMetricNoData             = errors.New("no data for this metric")
+	ErrMetricValEmptyField      = errors.New("empty field in metrics")
+	ErrMetricValWrongType       = errors.New("wrong type of metrics")
+	ErrMetricValValueIsNotFloat = errors.New("value is not float64")
 )
