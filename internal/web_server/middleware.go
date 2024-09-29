d4987331937d7a47f12e9a2a4a2259913bd0a266
Grifonhard <142656587+Grifonhard@users.noreply.github.com>
2024-09-29 21:38:05 +0300

    тест

--- internal/web_server/middleware.go
+++ internal/web_server/middleware.go
@@ -133,7 +133,7 @@ func DataExtraction() gin.HandlerFunc {
 			}
 		}
 
-		hvAE := c.Request.Header.Values("Accept-Encoding")
+		/*hvAE := c.Request.Header.Values("Accept-Encoding")
 		for _, h := range hvAE {
 			if h == "gzip" {
 				cW, err := NewCompressResponseWriter(c.Writer)
@@ -144,7 +144,7 @@ func DataExtraction() gin.HandlerFunc {
 				}
 				c.Writer = cW
 			}
-		}
+		}*/
 
 		if c.Request.Method == http.MethodPost && strings.Contains(c.Request.URL.Path, "/update") && strings.Contains(c.Request.Header.Get("Content-Type"), "application/json") {
 			c.Set(METRICTYPE, METRICTYPEJSON)
