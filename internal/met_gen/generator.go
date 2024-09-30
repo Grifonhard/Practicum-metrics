e5041e16b31370655753c6c5c8de5886841ff1a8
Igor <grifonhard@gmail.com>
2024-09-30 16:50:53 +0300

    навалил тестов

--- internal/met_gen/generator.go
+++ internal/met_gen/generator.go
@@ -12,15 +12,15 @@ type MetricsGenerator interface {
 }
 
 type MetGen struct {
-	metricsGauge   map[string]float64 //метрики float64
-	metricsCounter map[string]int64   //метрики int64
+	MetricsGauge   map[string]float64 //метрики float64
+	MetricsCounter map[string]int64   //метрики int64
 	mu sync.Mutex
 }
 
 func New() *MetGen {
 	var mg MetGen
-	mg.metricsGauge = make(map[string]float64)
-	mg.metricsCounter = make(map[string]int64)
+	mg.MetricsGauge = make(map[string]float64)
+	mg.MetricsCounter = make(map[string]int64)
 	return &mg
 }
 
@@ -30,36 +30,36 @@ func (mg *MetGen) Renew() error {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
 
-	mg.metricsGauge["Alloc"] = float64(memStats.Alloc)
-	mg.metricsGauge["BuckHashSys"] = float64(memStats.BuckHashSys)
-	mg.metricsGauge["Frees"] = float64(memStats.Frees)
-	mg.metricsGauge["GCCPUFraction"] = memStats.GCCPUFraction
-	mg.metricsGauge["GCSys"] = float64(memStats.GCSys)
-	mg.metricsGauge["HeapAlloc"] = float64(memStats.HeapAlloc)
-	mg.metricsGauge["HeapIdle"] = float64(memStats.HeapIdle)
-	mg.metricsGauge["HeapInuse"] = float64(memStats.HeapInuse)
-	mg.metricsGauge["HeapObjects"] = float64(memStats.HeapObjects)
-	mg.metricsGauge["HeapReleased"] = float64(memStats.HeapReleased)
-	mg.metricsGauge["HeapSys"] = float64(memStats.HeapSys)
-	mg.metricsGauge["LastGC"] = float64(memStats.LastGC)
-	mg.metricsGauge["Lookups"] = float64(memStats.Lookups)
-	mg.metricsGauge["MCacheInuse"] = float64(memStats.MCacheInuse)
-	mg.metricsGauge["MCacheSys"] = float64(memStats.MCacheSys)
-	mg.metricsGauge["MSpanInuse"] = float64(memStats.MSpanInuse)
-	mg.metricsGauge["MSpanSys"] = float64(memStats.MSpanSys)
-	mg.metricsGauge["Mallocs"] = float64(memStats.Mallocs)
-	mg.metricsGauge["NextGC"] = float64(memStats.NextGC)
-	mg.metricsGauge["NumForcedGC"] = float64(memStats.NumForcedGC)
-	mg.metricsGauge["NumGC"] = float64(memStats.NumGC)
-	mg.metricsGauge["OtherSys"] = float64(memStats.OtherSys)
-	mg.metricsGauge["PauseTotalNs"] = float64(memStats.PauseTotalNs)
-	mg.metricsGauge["StackInuse"] = float64(memStats.StackInuse)
-	mg.metricsGauge["StackSys"] = float64(memStats.StackSys)
-	mg.metricsGauge["Sys"] = float64(memStats.Sys)
-	mg.metricsGauge["TotalAlloc"] = float64(memStats.TotalAlloc)
-	mg.metricsGauge["RandomValue"] = rand.Float64()
+	mg.MetricsGauge["Alloc"] = float64(memStats.Alloc)
+	mg.MetricsGauge["BuckHashSys"] = float64(memStats.BuckHashSys)
+	mg.MetricsGauge["Frees"] = float64(memStats.Frees)
+	mg.MetricsGauge["GCCPUFraction"] = memStats.GCCPUFraction
+	mg.MetricsGauge["GCSys"] = float64(memStats.GCSys)
+	mg.MetricsGauge["HeapAlloc"] = float64(memStats.HeapAlloc)
+	mg.MetricsGauge["HeapIdle"] = float64(memStats.HeapIdle)
+	mg.MetricsGauge["HeapInuse"] = float64(memStats.HeapInuse)
+	mg.MetricsGauge["HeapObjects"] = float64(memStats.HeapObjects)
+	mg.MetricsGauge["HeapReleased"] = float64(memStats.HeapReleased)
+	mg.MetricsGauge["HeapSys"] = float64(memStats.HeapSys)
+	mg.MetricsGauge["LastGC"] = float64(memStats.LastGC)
+	mg.MetricsGauge["Lookups"] = float64(memStats.Lookups)
+	mg.MetricsGauge["MCacheInuse"] = float64(memStats.MCacheInuse)
+	mg.MetricsGauge["MCacheSys"] = float64(memStats.MCacheSys)
+	mg.MetricsGauge["MSpanInuse"] = float64(memStats.MSpanInuse)
+	mg.MetricsGauge["MSpanSys"] = float64(memStats.MSpanSys)
+	mg.MetricsGauge["Mallocs"] = float64(memStats.Mallocs)
+	mg.MetricsGauge["NextGC"] = float64(memStats.NextGC)
+	mg.MetricsGauge["NumForcedGC"] = float64(memStats.NumForcedGC)
+	mg.MetricsGauge["NumGC"] = float64(memStats.NumGC)
+	mg.MetricsGauge["OtherSys"] = float64(memStats.OtherSys)
+	mg.MetricsGauge["PauseTotalNs"] = float64(memStats.PauseTotalNs)
+	mg.MetricsGauge["StackInuse"] = float64(memStats.StackInuse)
+	mg.MetricsGauge["StackSys"] = float64(memStats.StackSys)
+	mg.MetricsGauge["Sys"] = float64(memStats.Sys)
+	mg.MetricsGauge["TotalAlloc"] = float64(memStats.TotalAlloc)
+	mg.MetricsGauge["RandomValue"] = rand.Float64()
 
-	mg.metricsCounter["PollCount"]++
+	mg.MetricsCounter["PollCount"]++
 
 	return nil
 }
@@ -69,10 +69,10 @@ func (mg *MetGen) Collect() (map[string]float64, map[string]int64, error) {
 	defer mg.mu.Unlock()
 	gg := make(map[string]float64)
 	cntr := make(map[string]int64)
-	for k, v := range mg.metricsGauge {
+	for k, v := range mg.MetricsGauge {
 		gg[k] = v
 	}
-	for k, v := range mg.metricsCounter {
+	for k, v := range mg.MetricsCounter {
 		cntr[k] = v
 	}
 	return gg, cntr, nil
