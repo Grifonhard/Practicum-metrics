db850a955f5726c48d0c68e3c9410809eb4c50a7
Grifonhard <142656587+Grifonhard@users.noreply.github.com>
2024-10-01 19:40:48 +0300

    test

--- internal/storage/fileio/fileio.go
+++ internal/storage/fileio/fileio.go
@@ -1,20 +1,19 @@
 package fileio
 
 import (
-	"bytes"
 	"encoding/gob"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
-	//"github.com/Grifonhard/Practicum-metrics/internal/logger"
+
+	"github.com/Grifonhard/Practicum-metrics/internal/logger"
 )
 
 type File struct {
 	fullpath string
-	file     io.ReadWriter
+	file     *os.File
 	mu       *sync.Mutex
 }
 
@@ -25,32 +24,27 @@ type Data struct {
 
 func New(path, filename string) (*File, error) {
 	var mu sync.Mutex
+	var file *os.File
+	var err error
+
 	fullpath := filepath.Join(path, filename)
 
 	if path != "" {
-		_, err := os.OpenFile(fullpath, os.O_RDWR|os.O_CREATE, 0666)
+		file, err = os.OpenFile(fullpath, os.O_RDWR|os.O_CREATE, 0666)
 		if err != nil && strings.Contains(err.Error(), "no such file or directory") {
-			fmt.Println("create directory")
+			logger.Info("create directory")
 			err = os.Mkdir(path, 0755)
 			if err != nil {
-				fmt.Println(err)
+				logger.Error(err)
 			}
 		} else {
-			fmt.Println(err)
+			logger.Error(err)
 		}
 	}
 
-	/*file, err := os.OpenFile(fullpath, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		return nil, err
-	}*/
-	var buf bytes.Buffer
-
-	fmt.Println(fullpath)
-
 	return &File{
 		fullpath: fullpath,
-		file:     &buf,
+		file:     file,
 		mu:       &mu,
 	}, nil
 }
@@ -58,8 +52,11 @@ func New(path, filename string) (*File, error) {
 func (f *File) Write(data *Data) error {
 	f.mu.Lock()
 	defer f.mu.Unlock()
+	if f.file == nil {
+		return nil
+	}
 
-	/*err := f.file.Truncate(0)
+	err := f.file.Truncate(0)
 	if err != nil {
 		return fmt.Errorf("fail truncate file: %w", err)
 	}
@@ -75,8 +72,7 @@ func (f *File) Write(data *Data) error {
 		return fmt.Errorf("failed to write data to file: %w", err)
 	}
 
-	return f.file.Sync()*/
-	return nil
+	return f.file.Sync()
 }
 
 func (f *File) Read() (map[string]float64, map[string][]float64, error) {
@@ -87,31 +83,38 @@ func (f *File) Read() (map[string]float64, map[string][]float64, error) {
 	data.ItemsGauge = make(map[string]float64)
 	data.ItemsCounter = make(map[string][]float64)
 
-	/*fileInfo, err := f.file.Stat()
+	if f.file == nil {
+		return data.ItemsGauge, data.ItemsCounter, nil
+	}
+
+	fileInfo, err := f.file.Stat()
 	if err != nil {
 		return nil, nil, fmt.Errorf("не удалось получить информацию о файле: %w", err)
 	}
 
 	if fileInfo.Size() == 0 {
 		return data.ItemsGauge, data.ItemsCounter, nil
-	}*/
+	}
 
 	decoder := gob.NewDecoder(f.file)
 
-	err := decoder.Decode(&data)
+	err = decoder.Decode(&data)
 	if err != nil {
-		/*logger.Error(fmt.Sprintf("Файл %s поврежден, удаляю...\n", f.fullpath))
+		logger.Error(fmt.Sprintf("Файл %s поврежден, удаляю...\n", f.fullpath))
 		removeErr := os.Remove(f.fullpath)
 		if removeErr != nil {
 			return nil, nil, fmt.Errorf("не удалось удалить поврежденный файл: %w", removeErr)
-		}*/
-		fmt.Println(err)
+		}
+		logger.Error(err)
+		return data.ItemsGauge, data.ItemsCounter, nil
 	}
 
 	return data.ItemsGauge, data.ItemsCounter, nil
 }
 
 func (f *File) Close() error {
-	//return f.file.Close()
-	return nil
+	if f.file == nil {
+		return nil
+	}
+	return f.file.Close()
 }
